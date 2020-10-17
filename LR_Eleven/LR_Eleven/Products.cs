253c8944ca4871f56273c9f493755aea5261f6c0
Alexey Svidnitskiy <lesha.svid@yandex.ru>
2020-10-17 03:46:09 +0300

    Лабораторная работа 11. Обработка данных в файлах

---
+++ LR_Eleven/LR_Eleven/Products.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace LR_Eleven
+{
+    enum CategoryType
+    {
+        A,
+        B,
+        C,
+        D,
+        Null
+    }
+    class Products
+    {
+        String ID { get; set; }
+        String Name { get; set;}
+        public CategoryType Category { get; set; }
+        public float Salary { get; set; }
+        public int Count { get; set; }
+        public float Price { get; set; }
+        public float Composition { get; set; }
+        public float Procent { get; set; }
+
+        public static Products Create(String str)
+        {
+            Products p = new Products();
+            string[] e = str.Split(',');
+            p.ID = e[0].Trim();
+            p.Name = e[1].Trim();
+            String tmp = e[2].Trim();
+            if (tmp == "A")
+                p.Category = CategoryType.A;
+            else if (tmp == "B")
+                p.Category = CategoryType.B;
+            else if (tmp == "C")
+                p.Category = CategoryType.C;
+            else if (tmp == "D")
+                p.Category = CategoryType.D;
+            else
+                p.Category = CategoryType.Null;
+            p.Salary = Convert.ToSingle(e[3].TrimStart('$').Replace('.',','));
+            p.Count = Convert.ToInt32(e[4].Trim());
+            p.Price = Convert.ToSingle(e[5].TrimStart('$').Replace('.', ','));
+            p.Composition = p.Price * p.Count;
+            p.Procent = p.Composition / p.Salary;
+            return p;
+        }
+
+        public override string ToString()
+        {
+            String s = string.Format(
+                "***********************************************************************************\n" +
+                "ID: {0}, Сотрудник: {1}, Категория товара: {2}\n" +
+                "З/П рабочего: {3}, Количество произведенных товаров: {4}\n" +
+                "Цена за единицу товара: {5}", ID, Name, CategoryToStr(Category) , Salary, Count, Price );
+
+            return s;
+        }
+
+        private static String CategoryToStr(CategoryType g)
+        {
+            if (g == CategoryType.A) return "A";
+            else if (g == CategoryType.B) return "B";
+            else if (g == CategoryType.C) return "C";
+            else if (g == CategoryType.D) return "D";
+            else return "Нет категории";
+        }
+    }
+}
---
+++ LR_Eleven/LR_Eleven/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Linq.Expressions;
+
+namespace LR_Eleven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* В файле содержатся сведения о производстве товаров работниками
+             * (ID, сотрудник, категория товара, з/п рабочего, количество
+             * произведенных товаров, цена за единицу товара).
+             * 1. Определите сколько рабочих получают меньше, чем
+             * вырабатывают продукции.
+             * 2. Количество единиц произведенной продукции без категории.
+             * 3. Суммарный объем в валюте произведенной продукции по каждой
+             * из 4-х категорий.
+             * 4. Количество сотрудников, получающих более 50% от суммы
+             * производимого продукта.
+             */
+
+            StreamReader FileIn = new StreamReader("lr11_27.csv");
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"lr11_output.txt");
+            Console.SetOut(new_out);
+#endif
+
+            List<Products> all = new List<Products>();
+            try
+            {
+                String line = FileIn.ReadLine();
+                while ((line = FileIn.ReadLine()) != null)
+                {
+                    all.Add(Products.Create(line));
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("****** Задача 1 ******");
+            int TasksCount = 0;
+            foreach (var p in all)
+            {
+                if (p.Salary<p.Composition)
+                {
+                    TasksCount++;
+                }
+            }
+            Console.WriteLine("Количество работников, которые получают меньше, чем вырабатывают продукции: {0}\n", TasksCount);
+
+            Console.WriteLine("****** Задача 2 ******");
+
+            float Tasks2Count = (from p in all
+                                 where (p.Category == CategoryType.Null) && (p.Count == 1)
+                                 select p).Count();
+            Console.WriteLine("Количество единиц произведенной продукции без категории: {0}\n", Tasks2Count);
+
+
+            Console.WriteLine("****** Задача 3 ******");
+
+            float Tasks3Count = (from p in all
+                                 select p.Composition).Sum();
+            var f = System.Globalization.CultureInfo.GetCultureInfo("en-us");
+            Console.WriteLine("Суммарный объем в валюте произведенной продукции по каждой из 4-х категорий: {0}\n", Tasks3Count.ToString("C", f));
+
+            Console.WriteLine("****** Задача 4 ******");
+            int Tasks4Count = 0;
+            foreach (var p in all)
+            {
+                if (p.Procent>0.5)
+                {
+                    Tasks4Count++;
+                }
+            }
+            Console.WriteLine("Количество сотрудников, получающих более 50% от суммы производимого продукта: {0}", Tasks4Count);
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#endif
+        }
+    }
+}
